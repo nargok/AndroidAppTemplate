769613ab02e1f6fb07a9950eec48f68beed1ef79
nargok <nargok@gmail.com>
2023-03-05 17:15:58 +0900

    display ProgressIndicator

--- app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleListView.kt
+++ app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleListView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,5 +33,9 @@ fun ArticleListView() {
                 Text(article.title)
             }
         }
+
+        if (viewModel.state.value.isLoading)
+            CircularProgressIndicator()
     }
+
 }
