3c3a67baef39b3a9c6c75edcb70dcc195bd883b7
nargok <nargok@gmail.com>
2023-03-05 17:27:11 +0900

    Setup navigation

--- app/src/main/java/com/example/qiitaapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/qiitaapp/presentation/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.example.qiitaapp.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.qiitaapp.domain.Article
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.qiitaapp.presentation.theme.QiitaAppTheme
-import com.example.qiitaapp.presentation.ui.theme.ArticleListView
+import com.example.qiitaapp.presentation.ui.theme.ArticleListScreen
+import com.example.qiitaapp.presentation.viewmodel.ArticlesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,11 +31,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ArticleListView()
+                    QiitaApp()
                 }
             }
         }
     }
+}
+
+
+@Composable
+private fun QiitaApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "articles") {
+        composable(route = "articles") {
+            val viewModel: ArticlesViewModel = hiltViewModel()
+            ArticleListScreen(
+                state = viewModel.state.value,
+                onItemClick = { id ->
+                    Log.d("Clicked Article ID", id)
+                }
+            )
+        }
+    }
+
 }
 
 @Composable
--- app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleListScreen.kt
+++ app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleListScreen.kt
@@ -1,24 +1,26 @@
 package com.example.qiitaapp.presentation.ui.theme
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.qiitaapp.presentation.viewmodel.ArticlesScreenState
 import com.example.qiitaapp.presentation.viewmodel.ArticlesViewModel
 
 @Composable
-fun ArticleListView() {
-    val viewModel: ArticlesViewModel = hiltViewModel()
-
+fun ArticleListScreen(
+    state: ArticlesScreenState,
+    onItemClick: (id: String) -> Unit,
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
@@ -29,12 +31,14 @@ fun ArticleListView() {
                 horizontal = 8.dp
             )
         ) {
-            items(viewModel.state.value.articles) { article ->
-                Text(article.title)
+            items(state.articles) { article ->
+                Text(article.title,
+                    modifier = Modifier.clickable { onItemClick(article.id) })
+
             }
         }
 
-        if (viewModel.state.value.isLoading)
+        if (state.isLoading)
             CircularProgressIndicator()
     }
 
