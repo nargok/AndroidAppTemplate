cae0abc47121d47dbf0f23a71bb87c9dd9a226cb
nargok <nargok@gmail.com>
2023-03-09 21:28:28 +0900

    favorite button in detail page

--- app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleDetailScreen.kt
+++ app/src/main/java/com/example/qiitaapp/presentation/ui/ArticleDetailScreen.kt
@@ -3,9 +3,13 @@ package com.example.qiitaapp.presentation.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.qiitaapp.presentation.viewmodel.ArticleDetailViewModel
 import com.google.accompanist.web.WebView
@@ -16,17 +20,31 @@ fun ArticleDetailsScreen() {
     val viewModel: ArticleDetailViewModel = hiltViewModel()
     val article = viewModel.state.value.article
 
-    if (article != null) {
-        val content = requireNotNull(article.htmlContent)
-        val webViewState = rememberWebViewStateWithHTMLData(data = content)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("記事詳細") },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+//                        Icon(Icons.Outlined.Favorite, "Favorite")
+                        Icon(Icons.Filled.FavoriteBorder, "Favorite")
+                    }
+                }
+            )
+        },
+    ) { contentPadding ->
+        if (article != null) {
+            val content = requireNotNull(article.htmlContent)
+            val webViewState = rememberWebViewStateWithHTMLData(data = content)
 
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-            WebView(state = webViewState)
+            Column(
+                modifier = Modifier
+                    .padding(contentPadding)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                WebView(state = webViewState)
 
+            }
         }
     }
 }
